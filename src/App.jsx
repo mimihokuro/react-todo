24718fcf83791c092a098cfb9295b8d5d68ddb6e
keetasan <keetasan7@gmail.com>
2022-05-08 22:21:24 +0900

    feat: ReactでTODOアプリを制作

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,91 @@
-import react, { useEffect, useState } from 'react';
-import ColorfulMessage from './components/ColorfulMessage';
+import react, { useState } from 'react';
+import './styles.css';
 
-const App = () => {
-  console.log('check');
-  const onClickCountUp = () => {
-    setNum(num + 1);
+export const App = () => {
+  const [todoText, setTodoText] = useState('');
+  const [incompleteTodos, setIncompleteTodos] = useState([]);
+  const [completeTodos, setCompleteTodos] = useState([]);
+
+  // レンダリングが走ってinputに文字入力できなくなる問題を解決
+  const onChangeTodoText = (e) => {
+    setTodoText(e.target.value);
   };
-  const onClickSwitch = () => {
-    setAlfa(!alfa);
+
+  // inputに入力した文字をTODOとして生成
+  const onClickAddTodos = () => {
+    if (todoText === '') return;
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    setTodoText('');
   };
-  const [num, setNum] = useState(0);
 
-  const [alfa, setAlfa] = useState(false);
+  // 生成されたTODOを削除
+  const onClickDelete = (index) => {
+    // 未完了の配列を取得
+    const newTodos = [...incompleteTodos];
+    // 未完了の配列からindexで指定したTODOを一つ除外
+    newTodos.splice(index, 1);
+    // 除外した後の配列を未完了の配列に反映⇒レンダリングされてincompleteTodosがHTMLに反映される
+    setIncompleteTodos(newTodos);
+  };
 
-  useEffect(() => {
-    if (num % 3 === 0) {
-      alfa || setAlfa(true);
-    } else {
-      alfa && setAlfa(false);
-    }
-  }, [num]);
+  // TODOを完了に移動
+  const onClickComplete = (index) => {
+    const newIncompleteTodos = [...incompleteTodos];
+    newIncompleteTodos.splice(index, 1);
+
+    const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
+    setIncompleteTodos(newIncompleteTodos);
+    setCompleteTodos(newCompleteTodos);
+  };
+
+  // TODOを未完了に移動
+  const onClickBack = (index) => {
+    const newCompleteTodos = [...completeTodos];
+    newCompleteTodos.splice(index, 1);
+
+    const newIncompleteTodos = [...incompleteTodos, completeTodos[index]];
+    setCompleteTodos(newCompleteTodos);
+    setIncompleteTodos(newIncompleteTodos);
+  };
 
   return (
     <>
-      <h1 style={{ color: 'red' }}>こんにちは</h1>
-      <ColorfulMessage color="blue">お元気ですか</ColorfulMessage>
-      <ColorfulMessage color="pink">元気です</ColorfulMessage>
-      <button onClick={onClickCountUp}>Count Up</button>
-      <p>{num}</p>
-      <button onClick={onClickSwitch}>on/off</button>
-      {alfa && <p>abcde</p>}
+      <div className="input-area">
+        <input
+          placeholder="TODOを入力"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
+        <button onClick={onClickAddTodos}>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
+        <ul>
+          {incompleteTodos.map((todo, index) => {
+            return (
+              <li key={todo} className="list-row">
+                <div>{todo}</div>
+                <button onClick={() => onClickComplete(index)}>完了</button>
+                <button onClick={() => onClickDelete(index)}>削除</button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="complete-area">
+        <p className="title">完了のTODO</p>
+        <ul>
+          {completeTodos.map((todo, index) => {
+            return (
+              <li key={todo} className="list-row">
+                <div>{todo}</div>
+                <button onClick={() => onClickBack(index)}>戻す</button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </>
   );
 };
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import reactDom from 'react-dom';
-import App from './App';
+import { App } from './App';
 
 reactDom.render(
   <StrictMode>
